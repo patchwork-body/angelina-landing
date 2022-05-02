ceb1510690afbd0fc172a973543762e3a522e147
gugfug <gugfug1@gmail.com>
2022-05-02 14:56:36 +0300

    feat: update page/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { AscentEmailInput } from "../components/ascent-email-input";
 import { StickyHeading } from "../components/sticky-heading";
 import { StickyPostageStamp } from "../components/sticky-postage-stamp";
 import { StickySlideContextProvider } from "../contexes/sticky-slide";
@@ -6,18 +7,44 @@ const Index = () => {
   return (
     <div className="relative">
       <StickySlideContextProvider className="bg-pink-100">
-        <StickyPostageStamp bgImageHref="/church.jpg" fillColor="#fff" />
+        <StickyPostageStamp
+          // className="absolute left-52 hidden md:block drop-shadow-lg"
+          bgImageHref="/church.jpg"
+          fillColor="#fff"
+        />
+
         <StickyHeading>Заголовок #2</StickyHeading>
       </StickySlideContextProvider>
 
       <StickySlideContextProvider className="bg-slate-300">
-        <StickyPostageStamp bgImageHref="/bridge.jpeg" fillColor="#fff" />
+        <StickyPostageStamp
+          // className="absolute left-52 hidden md:block drop-shadow-lg"
+          bgImageHref="/bridge.jpeg"
+          fillColor="#fff"
+        />
+
         <StickyHeading>Заголовок #3</StickyHeading>
       </StickySlideContextProvider>
 
       <StickySlideContextProvider className="bg-yellow-200">
-        <StickyPostageStamp bgImageHref="/wheel.jpeg" fillColor="#fff" />
+        <StickyPostageStamp
+          // className="absolute left-52 hidden md:block drop-shadow-lg"
+          bgImageHref="/wheel.jpeg"
+          fillColor="#fff"
+        />
+
         <StickyHeading>Заголовок #4</StickyHeading>
+      </StickySlideContextProvider>
+
+      <StickySlideContextProvider className="bg-yellow-200 grid place-items-center">
+        <StickyPostageStamp
+          // className="absolute left-52 hidden md:block drop-shadow-lg"
+          bgImageHref="/wheel.jpeg"
+          fillColor="#fff"
+        />
+
+        <StickyHeading>Заголовок #4</StickyHeading>
+        <AscentEmailInput />
       </StickySlideContextProvider>
     </div>
   );
