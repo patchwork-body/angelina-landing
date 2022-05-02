609558d0f56f14b1f07b5e1c9f4334fd27f0cde1
gugfug <gugfug1@gmail.com>
2022-05-02 14:52:11 +0300

    feat: add intersection observer to sticky slide context

--- contexes/sticky-slide/sticky-slide.context.tsx
+++ contexes/sticky-slide/sticky-slide.context.tsx
@@ -1,12 +1,18 @@
-import { createContext, FC, MutableRefObject, ReactNode, useRef } from "react";
+import { createContext, FC, MutableRefObject, ReactNode, useCallback, useRef } from "react";
 import classNames from "classnames";
 
 export type StickySlideContextValue = {
   slideRef: MutableRefObject<HTMLDivElement>;
+
+  setupIntersectionObserver: (
+    onIntersection: IntersectionObserverCallback,
+    options: IntersectionObserverInit,
+  ) => () => void;
 };
 
 export const StickySlideContext = createContext<StickySlideContextValue>({
   slideRef: null,
+  setupIntersectionObserver: () => () => void 0,
 });
 
 export type StickySlideContextProviderProps = {
@@ -20,8 +26,19 @@ export const StickySlideContextProvider: FC<StickySlideContextProviderProps> = (
 }) => {
   const slideRef = useRef<HTMLDivElement>(null);
 
+  const setupIntersectionObserver = useCallback(
+    (onIntersection: IntersectionObserverCallback, options: IntersectionObserverInit) => {
+      const target = slideRef.current;
+      const observer = new IntersectionObserver(onIntersection, options);
+      observer.observe(target);
+      return () => observer.unobserve(target);
+    },
+
+    [],
+  );
+
   return (
-    <StickySlideContext.Provider value={{ slideRef }}>
+    <StickySlideContext.Provider value={{ slideRef, setupIntersectionObserver }}>
       <div
         ref={slideRef}
         className={classNames("sticky top-0 overflow-hidden h-screen", className)}
