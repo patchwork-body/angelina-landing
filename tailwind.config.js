135b9febedcfa57e70ad1d4ce6c3787ef7aa4629
gugfug <gugfug1@gmail.com>
2022-05-02 15:05:13 +0300

    chore: remove deprecated 'purge' property in order to avoid tailwindcss warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 const tailwindConfig = {
   darkMode: "media",
-  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
