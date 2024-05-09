75d770673f24db4f61af0e70d6574678b1f659fc
Kirill Korotkov <kkorotkov@owner.one>
2024-05-09 19:43:56 +0200

    chore: Add initial project files and configurations

---
+++ uno.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+  ],
+  rules: [
+    ['font-sans', { 'font-family': "Poppins, sans-serif", 'font-optical-sizing': 'auto' }],
+  ],
+  theme: {
+    colors: {
+      'primary': '#32aa27' 
+    }
+  }
+});
